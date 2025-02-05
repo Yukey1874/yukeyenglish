6a2e530d49cd43d3d25da002adf3de8976037427
Yukey <95072981+Yukey1874@users.noreply.github.com>
2025-02-05 17:39:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '溪月Yukey', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '英语爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yukeyenglish.fun', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '论文翻译,英文沟通,英文基础', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '论文翻译,英文基础,翻译理论', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   //BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   //BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
