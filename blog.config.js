a5b8a5258e45dbc8b6b1a675625a630d09b1f79d
Yukey <95072981+Yukey1874@users.noreply.github.com>
2025-02-05 13:33:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '溪月Yukey', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '英语爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yukeyenglish.fun', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '英语，英语读书', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '论文翻译,英语学习', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   //BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   //BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '一个程序员的英语笔记',
+    '为技术人搭建从中文到英语的桥梁',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
