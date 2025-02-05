973ad89e2a6b1f3bc62e858e696205df439ea968
Yukey <95072981+Yukey1874@users.noreply.github.com>
2025-02-05 17:43:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '为技术人搭建从中文到英语的桥梁',
+    '为技术人搭建从中文到英文的桥梁',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
